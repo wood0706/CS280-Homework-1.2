53c9ebe96dc5377f8b28f2a3cf5facee28f3b46f
wood0706 <wood0706@gmail.com>
2017-03-06 14:50:57 +0800

    初始化專案

---
+++ CS280-Homework-1.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Homework_1._2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            pokemon pokemon = new pokemon();
+            pokemon.Number = "004";
+            pokemon.Name = "Charmander";
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 12.7f;
+            pokemon.Category = "Lizard";
+            pokemon.Abilities = "Blaze";
+            pokemon.Gender = "M";
+            Console.WriteLine("編號:{0},名字:{1},身高:{2},體重:{3},類別:{4},能力:{5},性別:{6}", pokemon.Number, pokemon.Name, pokemon.Height, pokemon.Weight, pokemon.Category, pokemon.Abilities, pokemon.Gender);
+        }
+    }
+}
